418a78920520bee5429928f621fad1d772e2d17c
comerc <comerc@mail.ru>
2015-01-11 14:48:24 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'comerc:autoform-fixtures',
   summary: 'Get fixtures data from SimpleSchema with AutoForm',
-  version: '1.1.2',
+  version: '1.1.3',
   git: 'https://github.com/comerc/meteor-autoform-fixtures.git'
 });
 
