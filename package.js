d3cf3b5fa0c565d895c8e193d11cb67743b12767
comerc <comerc@mail.ru>
2015-12-20 08:26:05 +0300

    compatible with autoform 5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'comerc:autoform-fixtures',
   summary: 'Get fixtures data from SimpleSchema with AutoForm',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/comerc/meteor-autoform-fixtures.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   api.use('coffeescript');
   api.use('momentjs:moment@2.8.4', 'client');
-  api.use('aldeed:autoform@4.0.0');
+  api.use('aldeed:autoform@5.0.0');
   api.addFiles([
     'autoform-fixtures.coffee',
   ], 'client');
